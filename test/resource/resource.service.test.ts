08ba392979013775f6e58bc1e44e25930044602d
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2024-01-04 19:44:56 +0900

    resource.service.test.ts 로 변경 및 이동

---
+++ test/resource/resource.service.test.ts
@@ -0,0 +1,102 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ResourceService } from '../../src/resource/resource.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Resource } from '../../src/resource/entity/resource.entity';
+import { NotFoundException } from '@nestjs/common';
+import { ResourceDto } from '../../src/resource/dto/resource.dto';
+
+describe('ResourceService', () => {
+    let resourceService: ResourceService;
+    let resourceRepository: Repository<Resource>;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                ResourceService,
+                {
+                    provide: getRepositoryToken(Resource),
+                    useClass: Repository,
+                },
+            ],
+        }).compile();
+
+        resourceService = module.get<ResourceService>(ResourceService);
+        resourceRepository = module.get<Repository<Resource>>(getRepositoryToken(Resource));
+    });
+
+
+    it('should be defined', () => {
+        expect(resourceService).toBeDefined();
+        expect(resourceRepository).toBeDefined();
+    });
+
+
+
+    describe('getAllResource', () => {
+        it('should return an array of resources', async () => {
+            const spyFn = jest.spyOn(resourceService, 'getAllResource').mockResolvedValueOnce([[],1])
+            const result = await resourceService.getAllResource();
+
+            expect(spyFn).toBeCalled();
+            expect(spyFn).toBeCalledTimes(1);
+            expect(spyFn).toBeCalledWith();
+            expect(result).toStrictEqual([[],1]);
+        });
+
+        it('resourceService.getAllResource should call resourceRepository.findAndCount',async () => {
+
+        });
+
+        it('findAndCount', () => {
+            const result = resourceRepository.findAndCount;
+            expect(typeof result).toBe('function');
+        })
+    });
+
+    describe('getResourceListWithVin', () => {
+        it('should return a list of resources with VIN information', async () => {
+            // TODO: Implement the test for getResourceListWithVin method
+        });
+    });
+
+    describe('getResource', () => {
+        it('should throw NotFoundException when resource is not found', async () => {
+            // TODO: Implement the test for getResource method when resource is not found
+        });
+
+        it('should return the resource when it exists', async () => {
+            // TODO: Implement the test for getResource method when resource is found
+        });
+    });
+
+    describe('searchResource', () => {
+        it('should return an array of resources matching the search criteria', async () => {
+            // TODO: Implement the test for searchResource method
+        });
+    });
+
+    describe('saveResource', () => {
+        it('should save a new resource and return the saved resource', async () => {
+            // TODO: Implement the test for saveResource method
+        });
+    });
+
+    describe('updateResource', () => {
+        it('should update an existing resource and return success message', async () => {
+            // TODO: Implement the test for updateResource method
+        });
+    });
+
+    describe('deleteResource', () => {
+        it('should delete an existing resource and return success message', async () => {
+            // TODO: Implement the test for deleteResource method
+        });
+    });
+
+    describe('userResource', () => {
+        it('should return an array of resources owned by the user', async () => {
+            // TODO: Implement the test for userResource method
+        });
+    });
+});
