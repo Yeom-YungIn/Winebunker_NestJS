1a5329c03d08e5960401d6413e229ccc0e599cae
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2024-03-23 18:42:29 +0900

    chore: renamed resource service spec

--- test/resource/resource.service.spec.ts
+++ test/resource/resource.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ResourceService } from '../../src/resource/resource.service';
-import {Repository} from 'typeorm';
+import { Repository } from 'typeorm';
 import { Resource } from '../../src/resource/entity/resource.entity';
 import {getRepositoryToken} from "@nestjs/typeorm";
+import {NotFoundException} from "@nestjs/common";
+
+
+
 
 describe('ResourceService', () => {
     let resourceService: ResourceService;
     let resourceRepository: Repository<Resource>;
+
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -23,19 +28,16 @@ describe('ResourceService', () => {
         resourceRepository = module.get<Repository<Resource>>(getRepositoryToken(Resource));
     });
 
-
-
     it('should be defined', () => {
         expect(resourceService).toBeDefined();
         expect(resourceRepository).toBeDefined();
     });
 
-
-
     describe('getAllResource', () => {
-        let resource;
+        let resource, spyFn;
         beforeEach(async () => {
             resource = new Resource();
+            spyFn = jest.spyOn(resourceRepository, 'findAndCount').mockResolvedValueOnce([[resource], 1])
         })
 
         it('getAllResource to be function', () => {
@@ -44,12 +46,12 @@ describe('ResourceService', () => {
         })
 
         it('resourceService.getAllResource should call resourceRepoitory.getALLResource', async () => {
-            const spyFn = jest.spyOn(resourceService, 'getAllResource').mockResolvedValueOnce([[resource], 1])
             const result = await resourceService.getAllResource();
             expect(spyFn).toBeCalled();
             expect(spyFn).toBeCalledTimes(1);
-            expect(spyFn).toBeCalledWith();
+            expect(spyFn).toBeCalledWith({"skip": 5, "take": 5});
             expect(result).toStrictEqual([[resource],1]);
+            expect(result).toBeInstanceOf(Object);
         });
     });
 });
