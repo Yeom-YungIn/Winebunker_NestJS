813954f277daa96569e2a0b781c8e201a77a4702
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2024-08-05 21:19:02 +0900

    feat: nestjs/swagger 적용

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import {ConfigType} from "./configs/config.type";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService<ConfigType>)
   const PORT = configService.getOrThrow('app.port', {infer: true})
+
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('WineBunker')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(PORT);
 
