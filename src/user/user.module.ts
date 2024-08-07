086f8ffdc22acddfa621b4d2e8d4120e7e11a06d
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2024-08-07 23:24:18 +0900

    feat: user module service, entity , controller 적용

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,6 +1,14 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./common/entity/user.entity";
+import { UserController } from "./user.controller";
+import { UserService } from "./user.service";
 
 @Module({
-  
+  imports: [
+    TypeOrmModule.forFeature([User])
+  ],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class UserModule {}
