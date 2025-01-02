dad73b2a2b934d6aa1f20eec35ecc1cebdbb83cb
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2025-01-02 23:15:07 +0900

    feat: 통계 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {typeORMConfig} from "./configs/typeorm.config";
 import { VinModule } from './vin/vin.module';
 import {ConfigModule} from "@nestjs/config";
+import { StatisticsModule } from './statistics/statistics.module';
 import appConfig from "./configs/app.config";
 import authConfig from "./auth/config/auth.config";
 @Module({
@@ -20,7 +21,8 @@ import authConfig from "./auth/config/auth.config";
     TypeOrmModule.forRoot(typeORMConfig),
     AuthModule,
     ResourceModule,
-    VinModule
+    VinModule,
+    StatisticsModule
   ],
   controllers: [],
   providers: [],
