4f28fd21cbd00f3055252257868876c8daf4f36e
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2025-03-01 23:28:13 +0900

    refactor: User Service exports

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,14 +1,13 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { User } from "./common/entity/user.entity";
-import { UserController } from "./user.controller";
-import { UserService } from "./user.service";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './common/entity/user.entity';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([User])
-  ],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
