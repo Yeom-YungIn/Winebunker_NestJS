30d43cd41589e45af7b81f38f2a6e07fa1181c41
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2025-03-01 23:28:35 +0900

    refactor: user entity imports modify

--- src/resource/resource.controller.ts
+++ src/resource/resource.controller.ts
@@ -1,13 +1,21 @@
-import {Body, Controller, Delete, Get, ParseUUIDPipe, Patch, Post, Query, UseGuards} from '@nestjs/common';
-import {ResourceService} from "./resource.service";
-import {ResourceDto} from "./dto/resource.dto";
-import {Resource} from "./entity/resource.entity";
-import {AuthGuard} from "@nestjs/passport";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { ResourceService } from './resource.service';
+import { ResourceDto } from './dto/resource.dto';
+import { Resource } from './entity/resource.entity';
+import { AuthGuard } from '@nestjs/passport';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
-import { GetUser, ResourceObjValidatePipe } from "@app/utils";
-import { User } from "../auth/common/entity/user.entity";
-
-
+import { GetUser, ResourceObjValidatePipe } from '@app/utils';
+import { User } from '../user/common/entity/user.entity';
 
 const NodeCache = require('node-cache');
 const cache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
@@ -16,74 +24,71 @@ const cache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
 @Controller('resource')
 @UseGuards(AuthGuard())
 export class ResourceController {
-    constructor(
-        private readonly resourceService: ResourceService
-    ) {
-    }
+  constructor(private readonly resourceService: ResourceService) {}
 
-    @Get()
-    @ApiOperation({summary: "리소스 목록 조회", description: "리소스 데이터만 조회"})
-    getAllResource(@Query('page') page: number = 1): Promise<[Resource[], number]> {
-        console.time('cache test')
-        const key: string = 'resource';
-        const cachedResource = cache.get(key);
-        if (cachedResource) {
-            return cachedResource;
-        } else {
-            const result:  Promise<[Resource[], number]> = this.resourceService.getAllResource(page);
-            cache.set(key, result);
-            return result;
-        }
+  @Get()
+  @ApiOperation({ summary: '리소스 목록 조회', description: '리소스 데이터만 조회' })
+  getAllResource(@Query('page') page: number = 1): Promise<[Resource[], number]> {
+    console.time('cache test');
+    const key: string = 'resource';
+    const cachedResource = cache.get(key);
+    if (cachedResource) {
+      return cachedResource;
+    } else {
+      const result: Promise<[Resource[], number]> = this.resourceService.getAllResource(page);
+      cache.set(key, result);
+      return result;
     }
+  }
 
-    @Get('/vin/list')
-    @ApiOperation({summary: "리소스 & 와인 정보 목록 조회", description: "리소스 목록 (와인 정보 포함) 조회"})
-    getResourceListWithVin(): Promise<Object> {
-        const start = Date.now()
-        const key: string = 'resource';
-        const cachedResource = cache.get(key);
-        if (cachedResource) {
-            const end = Date.now();
-            return cachedResource;
-        } else {
-            const result: Promise<Object> = this.resourceService.getResourceListWithVin()
-            cache.set(key, result, 60);
-            const end = Date.now();
-            return result;
-        }
+  @Get('/vin/list')
+  @ApiOperation({
+    summary: '리소스 & 와인 정보 목록 조회',
+    description: '리소스 목록 (와인 정보 포함) 조회',
+  })
+  getResourceListWithVin(): Promise<Object> {
+    const start = Date.now();
+    const key: string = 'resource';
+    const cachedResource = cache.get(key);
+    if (cachedResource) {
+      const end = Date.now();
+      return cachedResource;
+    } else {
+      const result: Promise<Object> = this.resourceService.getResourceListWithVin();
+      cache.set(key, result, 60);
+      const end = Date.now();
+      return result;
     }
+  }
 
-    @Get('/get')
-    getResource(@Query('id', ParseUUIDPipe) id: string): Promise<Resource> {
-        return this.resourceService.getResource(id);
-    }
+  @Get('/get')
+  getResource(@Query('id', ParseUUIDPipe) id: string): Promise<Resource> {
+    return this.resourceService.getResource(id);
+  }
 
-    @Get('/search')
-    searchResource(@Query('searchVal') searchVal: string): Promise<Resource[]> {
-        return this.resourceService.searchResource(searchVal);
-    }
+  @Get('/search')
+  searchResource(@Query('searchVal') searchVal: string): Promise<Resource[]> {
+    return this.resourceService.searchResource(searchVal);
+  }
 
-    @Post('/save')
-    @UseGuards(AuthGuard())
-    saveResource(
-        @Body() resourceDto: ResourceDto,
-        @GetUser() user: User
-    ): Promise<Object> {
-        return this.resourceService.saveResource(resourceDto, user)
-    }
+  @Post('/save')
+  @UseGuards(AuthGuard())
+  saveResource(@Body() resourceDto: ResourceDto, @GetUser() user: User): Promise<Object> {
+    return this.resourceService.saveResource(resourceDto, user);
+  }
 
-    @Patch('/update')
-    updateResource(@Body(ResourceObjValidatePipe) resourceDto: ResourceDto): Promise<Object> {
-        return this.resourceService.updateResource(resourceDto);
-    }
+  @Patch('/update')
+  updateResource(@Body(ResourceObjValidatePipe) resourceDto: ResourceDto): Promise<Object> {
+    return this.resourceService.updateResource(resourceDto);
+  }
 
-    @Delete('/delete')
-    deleteResource(@Body('id', ParseUUIDPipe) id: string): Promise<Object> {
-        return this.resourceService.deleteResource(id);
-    }
+  @Delete('/delete')
+  deleteResource(@Body('id', ParseUUIDPipe) id: string): Promise<Object> {
+    return this.resourceService.deleteResource(id);
+  }
 
-    @Get('/mypage')
-    userResource(@GetUser() user: User): Promise<Resource[]> {
-        return this.resourceService.userResource(user);
-    }
+  @Get('/mypage')
+  userResource(@GetUser() user: User): Promise<Resource[]> {
+    return this.resourceService.userResource(user);
+  }
 }
