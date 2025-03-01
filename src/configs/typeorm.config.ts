05825db248620f95077fee5cc567e46d43fcdbff
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2025-03-02 02:52:34 +0900

    refactor: typeorm naming strategy set

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,20 +1,22 @@
-import {TypeOrmModuleOptions} from "@nestjs/typeorm";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as config from 'config';
-import * as process from "process";
+import * as process from 'process';
 import * as dotenv from 'dotenv';
 const dbConfig = config.get('db');
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 const env = process.env.NODE_ENV || 'development';
 
 dotenv.config({ path: `.env.${env}` });
-export const typeORMConfig : TypeOrmModuleOptions = {
-    type: dbConfig.type,
-    host:  process.env.RDS_HOST ,
-    port:  Number(process.env.RDS_PORT) ,
-    database:  process.env.RDS_DATABASE || dbConfig.database,
-    username:  process.env.RDS_USERNAME || dbConfig.username,
-    password:  process.env.RDS_PASSWORD || dbConfig.password,
-    entities: ["dist/**/*.entity.js"],
-    synchronize: process.env.RDS_SYNC || dbConfig.synchronize,
-    autoLoadEntities: dbConfig.autoLoadEntities
-}
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: dbConfig.type,
+  host: process.env.RDS_HOST,
+  port: Number(process.env.RDS_PORT),
+  database: process.env.RDS_DATABASE || dbConfig.database,
+  username: process.env.RDS_USERNAME || dbConfig.username,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
+  entities: ['dist/**/*.entity.js'],
+  synchronize: process.env.RDS_SYNC || dbConfig.synchronize,
+  autoLoadEntities: dbConfig.autoLoadEntities,
+  namingStrategy: new SnakeNamingStrategy(),
+};
