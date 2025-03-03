e34ca228e9dded31b5b895f69180ab8d9175f5c0
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2025-03-03 16:10:10 +0900

    refactor: modify userName to name other services

--- src/auth/common/strategy/jwt.strategy.ts
+++ src/auth/common/strategy/jwt.strategy.ts
@@ -20,8 +20,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload) {
-    const { userName } = payload;
-    const user: User = await this.userRepository.findOneBy({ userName });
+    const { name } = payload;
+    const user: User = await this.userRepository.findOneBy({ name });
     if (!user) {
       throw new UnauthorizedException();
     }
--- src/resource/resource.service.ts
+++ src/resource/resource.service.ts
@@ -1,124 +1,113 @@
-import {Injectable, NotFoundException} from '@nestjs/common';
-import {InjectRepository} from "@nestjs/typeorm";
-import {Resource} from "./entity/resource.entity";
-import {Repository} from "typeorm";
-import {ResourceDto} from "./dto/resource.dto";
-import { User } from "../user/common/entity/user.entity";
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Resource } from './entity/resource.entity';
+import { Repository } from 'typeorm';
+import { ResourceDto } from './dto/resource.dto';
+import { User } from '../user/common/entity/user.entity';
 
 @Injectable()
 export class ResourceService {
-    constructor(
-        @InjectRepository(Resource)
-        private readonly resourceRepository: Repository<Resource>
-    ) {}
+  constructor(
+    @InjectRepository(Resource)
+    private readonly resourceRepository: Repository<Resource>,
+  ) {}
 
-    async getAllResource(page: number = 1): Promise<[Resource[], number]> {
-        const pageSize: number = 5;
-        return await this.resourceRepository.findAndCount({
-            skip: page * pageSize,
-            take: pageSize,
-        });
+  async getAllResource(page: number = 1): Promise<[Resource[], number]> {
+    const pageSize: number = 5;
+    return await this.resourceRepository.findAndCount({
+      skip: page * pageSize,
+      take: pageSize,
+    });
+  }
+
+  async getResourceListWithVin(): Promise<Object> {
+    return await this.resourceRepository
+      .createQueryBuilder('resource')
+      .innerJoinAndSelect('resource.vin', 'vin')
+      .addSelect('resource.id', 'id')
+      .addSelect('resource.price', 'price')
+      .addSelect('resource.store', 'store')
+      .addSelect('resource.purchaseDate', 'purchaseDate')
+      .addSelect('vin.vinNameKor', 'vinNameKor')
+      .getRawMany();
+  }
+
+  async getResource(id: string): Promise<Resource> {
+    const found = await this.resourceRepository.findOneBy({ id });
+
+    if (!found) {
+      throw new NotFoundException();
     }
+    return found;
+  }
 
+  async searchResource(searchVal: string): Promise<Resource[]> {
+    return await this.resourceRepository
+      .createQueryBuilder('resource')
+      .innerJoinAndSelect('resource.vin', 'vin')
+      .addSelect('resource.id', 'id')
+      .addSelect('resource.price', 'price')
+      .addSelect('resource.store', 'store')
+      .addSelect('resource.purchaseDate', 'purchaseDate')
+      .addSelect('vin.vinNameKor', 'vinNameKor')
+      .where('vin.vinNameKor Like :vinNameKor', { vinNameKor: `%${searchVal}%` })
+      .getRawMany();
+  }
 
-    async getResourceListWithVin(): Promise<Object> {
-        return await this.resourceRepository
-            .createQueryBuilder('resource')
-            .innerJoinAndSelect('resource.vin', 'vin')
-            .addSelect('resource.id', 'id')
-            .addSelect('resource.price', 'price')
-            .addSelect('resource.store', 'store')
-            .addSelect('resource.purchaseDate', 'purchaseDate')
-            .addSelect('vin.vinNameKor', 'vinNameKor')
-            .getRawMany();
+  async saveResource(resourceDto: ResourceDto, user: User): Promise<Object> {
+    const { vinSn, vintage, price, store, capacity, description, purchaseDate } = resourceDto;
+
+    const resource = this.resourceRepository.create({
+      vinSn,
+      vintage,
+      price,
+      store,
+      capacity,
+      description,
+      purchaseDate,
+      issued: new Date(),
+      modified: new Date(),
+      publisherId: user.name,
+    });
+
+    return this.resourceRepository.save(resource);
+  }
+
+  async updateResource(resourceDto: ResourceDto): Promise<Object> {
+    const { id, price, store, capacity, description, purchaseDate } = resourceDto;
+
+    const updateResource = await this.resourceRepository
+      .createQueryBuilder()
+      .update(Resource)
+      .set({
+        price,
+        store,
+        capacity,
+        description,
+        purchaseDate,
+      })
+      .where({
+        id,
+      })
+      .execute();
+    if (updateResource.affected) {
+      return { result: 'success' };
     }
+  }
 
-    async getResource(id: string): Promise<Resource> {
-        const found = await this.resourceRepository.findOneBy({id});
+  async deleteResource(id: string): Promise<Object> {
+    const found = await this.getResource(id);
 
-        if (!found) {
-            throw new NotFoundException();
-        }
-        return found;
+    const deleteResource = await this.resourceRepository.delete({ id: found.id });
+    if (deleteResource.affected) {
+      return { result: 'success' };
     }
+  }
 
-    async searchResource(searchVal: string): Promise<Resource[]> {
-        return await this.resourceRepository
-            .createQueryBuilder('resource')
-            .innerJoinAndSelect('resource.vin', 'vin')
-            .addSelect('resource.id', 'id')
-            .addSelect('resource.price', 'price')
-            .addSelect('resource.store', 'store')
-            .addSelect('resource.purchaseDate', 'purchaseDate')
-            .addSelect('vin.vinNameKor', 'vinNameKor')
-            .where('vin.vinNameKor Like :vinNameKor',{vinNameKor: `%${searchVal}%`})
-            .getRawMany();
-    }
-
-    async saveResource(resourceDto: ResourceDto, user: User): Promise<Object> {
-        const {
-            vinSn,
-            vintage,
-            price,
-            store,
-            capacity,
-            description,
-            purchaseDate
-        } = resourceDto;
-
-        const resource = this.resourceRepository.create({
-            vinSn,
-            vintage,
-            price,
-            store,
-            capacity,
-            description,
-            purchaseDate,
-            issued: new Date(),
-            modified: new Date(),
-            publisherId: user.userName
-        })
-
-        return this.resourceRepository.save(resource)
-    }
-
-    async updateResource(resourceDto: ResourceDto): Promise<Object> {
-        const {
-            id,
-            price,
-            store,
-            capacity,
-            description,
-            purchaseDate
-        } = resourceDto;
-
-        const updateResource = await this.resourceRepository.createQueryBuilder().update(Resource).set({
-            price,
-            store,
-            capacity,
-            description,
-            purchaseDate
-        }).where({
-            id
-        }).execute();
-        if (updateResource.affected) {
-            return {result: "success"};
-        }
-    }
-
-    async deleteResource(id: string): Promise<Object> {
-        const found = await this.getResource(id);
-
-        const deleteResource = await this.resourceRepository.delete({id: found.id});
-        if (deleteResource.affected) {
-            return {result: "success"}
-        }
-    }
-
-    async userResource(user: User): Promise<Resource[]> {
-        const {userName} = user;
-        return await this.resourceRepository.find({
-            where: {publisherId: userName}
-        })
-    }
+  async userResource(user: User): Promise<Resource[]> {
+    const { name } = user;
+    return await this.resourceRepository.find({
+      where: { publisherId: name },
+    });
+  }
 }
