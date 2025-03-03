4e759bfe24d9954c57a489c7a5303cc6c14cbffb
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2025-03-03 16:06:08 +0900

    refactor: userController annotation add

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,14 @@
-import { Body, Post, ValidationPipe } from "@nestjs/common";
-import { UserService } from "./user.service";
-import { UserSignUpDTO } from "./common/dto/user.request.dto";
-import { SignUpUserResponseDTO } from "./common/dto/user.response.dto";
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserSignUpDTO } from './common/dto/user.request.dto';
+import { SignUpUserResponseDTO } from './common/dto/user.response.dto';
 
+@Controller('user')
 export class UserController {
-  constructor(
-    private readonly userService: UserService
-  ) {
-  }
+  constructor(private readonly userService: UserService) {}
 
   @Post()
   signUp(@Body(ValidationPipe) userSignUpDTO: UserSignUpDTO): Promise<SignUpUserResponseDTO> {
     return this.userService.signUp(userSignUpDTO);
   }
-
 }
