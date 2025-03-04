8a0443ea12c98c6ab12fcd21d547112a69aa14bf
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2025-03-04 22:34:44 +0900

    feat: global validation pipe 추가

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import {ConfigType} from "./configs/config.type";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ConfigType } from './configs/config.type';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService<ConfigType>)
-  const PORT = configService.getOrThrow('app.port', {infer: true})
+  const configService = app.get(ConfigService<ConfigType>);
+  const PORT = configService.getOrThrow('app.port', { infer: true });
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('WineBunker')
